6ec993c589bbd435064fbeb3516547f7dca22e3d
Focus <Nature.Balance@proton.me>
2026-02-17 11:08:19 +0200

    Create FCC_2026-02-17.py

---
+++ FCC_2026-02-17.py
@@ -0,0 +1,19 @@
+def check_eligibility(athlete_weights, sled_weight):
+    min_sled_weights = {
+        1: 165,
+        2: 170,
+        4: 210
+    }
+    max_weights = {
+        1: 247,
+        2: 390,
+        4: 640
+    }
+    athlete_count = len(athlete_weights)
+    if sled_weight < min_sled_weights[athlete_count]:
+        return "Not Eligible"
+
+    total = sled_weight + sum(athlete_weights)
+    if total > max_weights[athlete_count]:
+        return "Not Eligible"
+    return "Eligible"
