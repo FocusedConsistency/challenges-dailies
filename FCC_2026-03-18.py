e0841faa46895959a1fa62a8c8d2da1e4b0c03a9
Focus <Nature.Balance@proton.me>
2026-03-19 00:12:57 +0200

    Update FCC_2026-03-18.py
    Refactored for readability.

--- FCC_2026-03-18.py
+++ FCC_2026-03-18.py
@@ -1,13 +1,16 @@
 def largest_number(s):
     numbers = []
     current = []
+    
+    def append_current():
+        num = "".join(current)
+        numbers.append(float(num))
+        
     for char in s:
         if char.isdecimal() or char in "-.":
             current.append(char)
         else:
-            num = "".join(current)
-            numbers.append(float(num))
+            append_current()
             current.clear()
-    num = "".join(current)
-    numbers.append(float(num))
+    append_current()
     return max(numbers)
