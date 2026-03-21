59badbd4370003e9dfc38cb9e460243b7a6622f6
Focus <Nature.Balance@proton.me>
2026-03-21 23:45:06 +0200

    Update FCC_2026-03-21.py

--- FCC_2026-03-21.py
+++ FCC_2026-03-21.py
@@ -1,5 +1,5 @@
 def decode_qr(qr_code):
-    code = [[0 for _ in range(len(qr_code[0]))] for _ in range(len(qr_code))]
+    code = [[0 for _ in range(6)] for _ in range(6)]
     for i in range(len(code)):
         for j in range(len(code[0])):
             code[i][j] = int(qr_code[i][j])
@@ -9,33 +9,33 @@ def decode_qr(qr_code):
 
     for line in rotated:
         print(line)
-    print("INITIAL")
+    print("Initial matrix")
 
-    while not (int(code[0][0]) and int(code[0][1]) and int(code[0][-2]) and int(code[0][-1]) and int(code[1][0]) and int(code[1][1]) and int(code[1][-2]) and int(code[1][-1]) and int(code[-2][0]) and int(code[-2][1]) and int(code[-1][0]) and int(code[-1][1])) and rotations < 4:
+    corners_to_skip = [(0,0), (0,1), (0,4), (0,5), (1,0), (1,1), (1,4), (1,5), (4,0), (4,1), (5,0), (5,1)]
+    while not all(code[i][j] for i, j in corners_to_skip) and rotations < 4:
         for i in range(len(code)):
             for j in range(len(code[0])):
                 rotated[j][-1-i] = int(code[i][j])
                 print(rotated[j])
-            print("---")
+            print("------------------")
         code = [row[:] for row in rotated]
         rotations += 1
-        print("Rotation finished--------------")
-    print(rotations)
+        print(f"Rotation {rotations} finished")
+        print("------------------")
 
     result = []
-    skipped = [(0,0), (0,1), (0,4), (0,5), (1,0), (1,1), (1,4), (1,5), (4,0), (4,1), (5,0), (5,1)]
     for i in range(len(code)):
         for j in range(len(code[0])):
-            if not (i,j) in skipped:
+            if not (i,j) in corners_to_skip:
                 result.append(str(rotated[i][j]))
-    print(''.join(result))
+
     return ''.join(result)
 
-decode_qr([
+print(decode_qr([
     "011011", 
     "101011", 
     "101000", 
     "100010", 
     "110011", 
-    "111011"])
+    "111011"]))
 # 010001000100010101010110
