281350aa38238c63b8c946150eb5e86b5a4a3548
Focus <Nature.Balance@proton.me>
2026-03-29 23:19:22 +0300

    Create FCC_2026-03-29.py

---
+++ FCC_2026-03-29.py
@@ -0,0 +1,24 @@
+def is_valid_isbn10(s):
+    digits = ''.join(s.split("-"))
+    if len(s) != 10:
+        return False
+
+    total = 0
+    for i, digit in enumerate(digits[:-1]):
+        if not digit.isdecimal():
+            return False
+        else:
+            total += int(digit) * (i+1)
+    
+    last_digit = digits[-1]
+    if not (last_digit.isdecimal() or last_digit == "X"):
+        return False
+    elif last_digit.isdecimal():
+        total += int(last_digit) * 10
+    elif last_digit == "X":
+        total += 10 * 10
+
+    return not total % 11
+
+is_valid_isbn10("0-306-40615-2")
+# True
