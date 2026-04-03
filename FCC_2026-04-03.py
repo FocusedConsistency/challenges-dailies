1dd23ab8a062fd393442ed53955280d52237a07f
Focus <Nature.Balance@proton.me>
2026-04-04 00:30:09 +0300

    Update FCC_2026-04-03.py

--- FCC_2026-04-03.py
+++ FCC_2026-04-03.py
@@ -3,19 +3,17 @@ def get_browser_history(commands):
     history = []
     for comm in commands:
         if comm == "Back":
-            if index != 0:
+            if index > 0:
                 index -= 1
         elif comm == "Forward":
-            if index != len(history) - 1:
+            if index < len(history) - 1:
                 index += 1
         else:
             index += 1
-            if index < len(history):
-                history[index] = comm
-            else:
-                history.append(comm)
+            history = history[:index]
+            history.append(comm)
 
     return [history, index]
 
-get_browser_history(["example.com", "example.com/about", "example.com/contact", "example.com/blog", "Back", "Back", "Forward", "freecodecamp.org"])
+print(get_browser_history(["example.com", "example.com/about", "example.com/contact", "example.com/blog", "Back", "Back", "Forward", "freecodecamp.org"]))
 # [["example.com", "example.com/about", "example.com/contact", "freecodecamp.org"], 3]
