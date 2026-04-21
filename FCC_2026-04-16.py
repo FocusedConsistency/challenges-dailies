afc9f52e388bd78024e0dd693ae05e657b63227a
Focus <Nature.Balance@proton.me>
2026-04-21 17:43:43 +0300

    Update FCC_2026-04-16.py
    Refactored

--- FCC_2026-04-16.py
+++ FCC_2026-04-16.py
@@ -5,22 +5,25 @@ def do_math(s):
     for c in s:
         if c.isdecimal():
             number += c
-            if len(equation) > 0 and non_digit_count > 0:
-                if non_digit_count % 2 == 0:
-                    equation.append("+")
-                else:
-                    equation.append("-")
+            # Add operator based on non-digit count parity
+            if equation and non_digit_count > 0:
+                equation.append("+" if non_digit_count % 2 == 0 else "-")
                 non_digit_count = 0
         else:
-            if len(number) > 0:
+            # Process accumulated number
+            if number:
                 equation.append(str(int(number)))
                 number = ""
             non_digit_count += 1
-    if len(number) > 0:
+
+    if number:
         equation.append(str(int(number)))
-    print(''.join(equation))
-    print(eval(''.join(equation)))
-    return eval(''.join(equation))
+
+    expr = ''.join(equation)
+    print(expr)
+    result = eval(expr) if equation else 0
+    print(result)
+    return result
 
 do_math("a.67,1$lk6ldf34@#LD@]2d32d2'2l3,@l3L#@2gh35s09if=df#$t9sm49t0df3$^%[vc;:0:4mt")
 # 67-1-6-34+2-32-2-2-3-3-2+35-9+9+49-0+3+0-4
